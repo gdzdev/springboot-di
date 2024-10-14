57dd640e699331462c7cfeb1ccf7d9b8fece2739
aleksGdz <aleksgaldamez2002@gmail.com>
2024-10-13 22:43:55 -0600

    initial commit

---
+++ src/test/java/org/gdzdev/springboot/di/app/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gdzdev.springboot.di.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
