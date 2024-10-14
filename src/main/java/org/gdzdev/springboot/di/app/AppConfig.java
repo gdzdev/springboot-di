98cd13ebed2a57701aa0cd07b9026b4cb841e33c
aleksGdz <aleksgaldamez2002@gmail.com>
2024-10-13 23:10:44 -0600

    Documented AppConfig class

--- src/main/java/org/gdzdev/springboot/di/app/AppConfig.java
+++ src/main/java/org/gdzdev/springboot/di/app/AppConfig.java
@@ -7,12 +7,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
+/**
+ * Clase de configuración de Spring que define los beans del contenedor de IoC.
+ * <p>
+ * Esta clase se utiliza para configurar el acceso a los recursos y crear instancias de los repositorios.
+ * </p>
+ */
 @Configuration
 public class AppConfig {
 
     @Value("classpath:json/product.json")
     private Resource resource;
 
+    /**
+     * Crea un bean del repositorio que carga productos desde un archivo JSON.
+     *
+     * @return Instancia de {@link ProductRepository} que utiliza datos de un archivo JSON.
+     */
     @Bean("repoJson")
     public ProductRepository repositoryJson() {
         return new ProductRepositoryJson(this.resource);
