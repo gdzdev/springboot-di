79d39b11a22783b5b2fda050dc7f3cfae66e3b91
aleksGdz <aleksgaldamez2002@gmail.com>
2024-10-13 23:10:10 -0600

    Documented ProductService interface and ProductServiceImpl

--- src/main/java/org/gdzdev/springboot/di/app/services/ProductService.java
+++ src/main/java/org/gdzdev/springboot/di/app/services/ProductService.java
@@ -4,9 +4,26 @@ import org.gdzdev.springboot.di.app.models.Product;
 
 import java.util.List;
 
+/**
+ * Interfaz que define los métodos de servicio para la gestión de productos.
+ * <p>
+ * Proporciona operaciones de negocio relacionadas con los productos, actuando como intermediario entre el controlador y el repositorio.
+ * </p>
+ */
 public interface ProductService {
 
+        /**
+         * Recupera todos los productos disponibles.
+         *
+         * @return Lista de objetos {@link Product}.
+         */
         List<Product> findAllProducts();
 
+        /**
+         * Busca un producto específico utilizando su identificador único.
+         *
+         * @param id Identificador único del producto a buscar.
+         * @return Objeto {@link Product} correspondiente al ID proporcionado, o null si no se encuentra.
+         */
         Product findProductById(Long id);
 }
--- src/main/java/org/gdzdev/springboot/di/app/services/ProductServiceImpl.java
+++ src/main/java/org/gdzdev/springboot/di/app/services/ProductServiceImpl.java
@@ -8,18 +8,36 @@ import org.springframework.stereotype.Service;
 import org.gdzdev.springboot.di.app.models.Product;
 import org.gdzdev.springboot.di.app.repositories.ProductRepository;
 
+/**
+ * Implementación del servicio de productos que maneja la lógica de negocio.
+ * <p>
+ * Esta clase interactúa con el repositorio para realizar operaciones sobre los productos,
+ * utilizando la inyección de dependencias para obtener la implementación del repositorio.
+ * </p>
+ */
 @Service
 public class ProductServiceImpl implements ProductService {
 
         @Autowired
-        @Qualifier("repoJson") // elegir por el nombre del Bean en el contendor de IoC
+        @Qualifier("repoJson") // Se elige la implementación de ProductRepository por su nombre en el contenedor de IoC
         private ProductRepository repository;
 
+        /**
+         * Recupera todos los productos disponibles mediante el repositorio.
+         *
+         * @return Lista de objetos {@link Product}.
+         */
         @Override
         public List<Product> findAllProducts() {
                 return repository.findAll();
         }
 
+        /**
+         * Busca un producto específico utilizando su identificador único.
+         *
+         * @param id Identificador único del producto a buscar.
+         * @return Objeto {@link Product} correspondiente al ID proporcionado, o null si no se encuentra.
+         */
         @Override
         public Product findProductById(Long id) {
                 return repository.findById(id);
